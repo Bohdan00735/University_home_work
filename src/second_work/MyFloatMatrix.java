29c6008a672834ac990cccaea4b827c14d03c65b
Bohdan <bodya00735@gmail.com>
2019-02-26 09:09:36 +0200

    final ver of second work

--- src/second_work/MyFloatMatrix.java
+++ src/second_work/MyFloatMatrix.java
@@ -5,46 +5,61 @@ import java.util.Scanner;
 
 public class MyFloatMatrix {
     private float [][] thisMatrix;
-    private int thisDimension;
-
-    MyFloatMatrix(int dimension){
-        thisDimension = dimension;
-    }
+    private int thisRows;
+    private int thisColumn;
 
     public float[][] getThisMatrix() {
         return thisMatrix;
+    }
+
+    public int getColumn() {
+        return thisColumn;
+    }
+
+    public int getRows() {
+        return thisRows;
     }
 
     float getElem(int x, int y){
         return thisMatrix[x][y];
     }
 
-    void setRandom(){
+    void setRandom(Scanner scanner){
 
-        thisMatrix  = new float[thisDimension][thisDimension];
+        thisRows = checkForInput("row", scanner);
+        thisColumn = checkForInput("column", scanner);
 
-        Random random = new Random(System.currentTimeMillis());
+        thisMatrix  = new float[thisRows][thisColumn];
 
-        for (int i = 0; i < thisDimension; i++){
-            for (int j = 0; j < thisDimension; j++){
-                thisMatrix[i][j] = random.nextFloat();
+        int min = checkForInput("min of diapason", scanner);
+        int max = checkForInput("max of diapason", scanner);
+        int dimmension = max-min;
+
+        Random random = new Random();
+
+        for (int i = 0; i < thisRows; i++){
+            for (int j = 0; j < thisColumn; j++){
+                thisMatrix[i][j] = min + (float)random.nextInt(dimmension +1);
             }
         }
     }
 
     void setThisMatrix(Scanner scanner){
-        thisMatrix = new float[thisDimension][thisDimension];
+        thisRows = checkForInput("row", scanner);
+        thisColumn = checkForInput("column", scanner);
+        thisMatrix = new float[thisRows][thisColumn];
 
-        for (int i = 0; i < thisDimension; i ++ ){
-            for (int j = 0; j < thisDimension; j++){
-                thisMatrix[i][j] = checkForInput(String.format("position %d : %d", thisDimension+1, thisDimension+1), scanner);
+        for (int i = 0; i < thisRows; i ++ ){
+            for (int j = 0; j < thisColumn; j++){
+                thisMatrix[i][j] = checkForInput(String.format("position %d : %d", thisRows+1, thisColumn+1), scanner);
             }
         }
     }
 
-    void setThisMatrix(float[][]matrix){
+    void setThisMatrix(float[][]matrix, int column, int row){
         thisMatrix = matrix;
-        thisDimension = matrix.length;
+        thisRows = row;
+        thisColumn = column;
     }
 
     private static int checkForInput(String name_of_varible, Scanner scanner){
@@ -53,26 +68,30 @@ public class MyFloatMatrix {
             if(scanner.hasNextInt() ){
                 return scanner.nextInt();
             }else {
-                System.out.println("please input only int ");
+                System.out.println("please input only int");
             }
         }
     }
 
     float[][] multiplicateOfMatrix(MyFloatMatrix anotherMatrix){
-        float [][] newMatrix = new float[thisDimension][thisDimension];
-        for (int i = 0; i < thisDimension; i++){
-            for (int j = 0; j < thisDimension; j ++){
-                for (int k = 0; k < thisDimension; k++){
-                    newMatrix[i][j]+=thisMatrix[i][k]*anotherMatrix.getElem(j,k);
+        float [][] newMatrix = new float[thisRows][thisColumn];
+        if (thisRows != anotherMatrix.getColumn()){
+            System.out.println("rows of first should be == column of second");
+            System.exit(1);
+        }else {
+            for (int i = 0; i < thisRows; i++){
+                for (int j = 0; j < thisColumn; j ++){
+                    for (int k = 0; k < thisRows; k++){
+                        newMatrix[i][j]+=thisMatrix[i][k]*anotherMatrix.getElem(j,k);
+                    }
                 }
             }
-        }
-        return newMatrix;
+        }return newMatrix;
     }
 
     void printMatrix(){
-        for (int i = 0; i < thisDimension; i++){
-            for (int j = 0; j < thisDimension; j ++ ){
+        for (int i = 0; i < thisRows; i++){
+            for (int j = 0; j < thisColumn; j ++ ){
                 System.out.print(thisMatrix[i][j]);
             }
             System.out.println(" ");
@@ -81,9 +100,9 @@ public class MyFloatMatrix {
 
     float calcSumOfLowestElemInRows(){
         float sum = 0;
-        for (int i = 0; i < thisDimension; i ++ ){
+        for (int i = 0; i < thisRows; i ++ ){
             float min = 10000;
-            for (int j = 0; j < thisDimension; j ++){
+            for (int j = 0; j < thisColumn; j ++){
                 if (thisMatrix[i][j]< min){
                     min = thisMatrix[i][j];
                 }
--- src/second_work/StartWorkWithMatrix.java
+++ src/second_work/StartWorkWithMatrix.java
@@ -5,18 +5,16 @@ import java.util.Scanner;
 public class StartWorkWithMatrix {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int dimension = checkForInput("dimension", scanner);
-
-        MyFloatMatrix firstMatrix = new MyFloatMatrix(dimension);
-        MyFloatMatrix secondMatrix = new MyFloatMatrix(dimension);
+        MyFloatMatrix firstMatrix = new MyFloatMatrix();
+        MyFloatMatrix secondMatrix = new MyFloatMatrix();
 
         System.out.println("press \"1\"if you want to set matrix random \n " +
                 "press \'2\' if you want matrix by yourself");
 
         String first_in = scanner.next();
         if (first_in.equals("1")){
-            firstMatrix.setRandom();
-            secondMatrix.setRandom();
+            firstMatrix.setRandom(scanner);
+            secondMatrix.setRandom(scanner);
 
         }else if (first_in.equals("2")){
             System.out.println("fill first matrix");
@@ -28,20 +26,9 @@ public class StartWorkWithMatrix {
             System.out.println("unresolved command");
         }
 
-        MyFloatMatrix resultMatrix = new MyFloatMatrix(dimension);
-        resultMatrix.setThisMatrix(firstMatrix.multiplicateOfMatrix(secondMatrix));
+        MyFloatMatrix resultMatrix = new MyFloatMatrix();
+        resultMatrix.setThisMatrix(firstMatrix.multiplicateOfMatrix(secondMatrix), firstMatrix.getRows(), secondMatrix.getColumn());
         resultMatrix.printMatrix();
         System.out.println("sum of min element each row is "+ resultMatrix.calcSumOfLowestElemInRows());
-    }
-
-    private static int checkForInput(String name_of_varible, Scanner scanner){
-        System.out.printf("please input %s type: int", name_of_varible);
-        while(true){
-            if(scanner.hasNextInt() ){
-                return scanner.nextInt();
-            }else {
-                System.out.println("please input only int ");
-            }
-        }
     }
 }
