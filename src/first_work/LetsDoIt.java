001d433665503320836ef0b7683038173f0ff339
Bogdan <bohdan00735@gmail.com>
2019-03-19 18:47:03 +0200

    шоош

--- src/first_work/LetsDoIt.java
+++ src/first_work/LetsDoIt.java
@@ -1,15 +1,16 @@
 package first_work;
 
+
 import java.util.Scanner;
 
-public class lets_do_it {
+public class LetsDoIt {
     public static void main(String[] args) {
-        int first_oper = 8119%2;
+        int firstOper = 8119%2;
         double constant= 8119%3;
-        int second_operation = 8119%5;
-        int type_of_index = 8119%7;
+        int secondOperation = 8119%5;
+        int typeOfIndex = 8119%7;
         double result = 0;
-        int count_dev_by_zero = 0;
+        int countDevByZero = 0;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input n");
@@ -17,21 +18,21 @@ public class lets_do_it {
         System.out.println("Input m");
         double m = scanner.nextDouble();
 
-        System.out.println("First_operator: -, because c2 = " + first_oper);
-        System.out.println("First_operator: -, because c5 = " + second_operation);
-        System.out.println("type of index is double , because c7 = " + type_of_index);
+        System.out.println("First_operator: -, because c2 = " + firstOper);
+        System.out.println("First_operator: -, because c5 = " + secondOperation);
+        System.out.println("type of index is double , because c7 = " + typeOfIndex);
         System.out.println("const = " + constant);
 
         for (double i =1; i < n;i++){
             for (double j = 1;j < m;j++){
                 if (i - constant == 0.0){
-                    count_dev_by_zero++;
+                    countDevByZero++;
                 }else {
                     result += (i-j)/(i-constant);
                 }
             }
         }
         System.out.println("result = " + result);
-        System.out.println("/ by zero has happened : " + count_dev_by_zero + " times");
+        System.out.println("/ by zero has happened : " + countDevByZero + " times");
     }
 }
